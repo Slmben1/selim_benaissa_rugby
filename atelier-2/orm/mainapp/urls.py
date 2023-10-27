a7993a25cbe882f16110bb5da69135a5f3586c53
Selim B <selim.benaissa@edu.esiee-it.fr>
2023-10-27 16:23:17 +0200

    affichage page en savoir plus

--- atelier-2/orm/mainapp/urls.py
+++ atelier-2/orm/mainapp/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 # Import des URLs de l'interface d'administration
 from django.contrib import admin
 # Import des vues qui sont déclarées dans leur propre module (dossier)
-from .views import HomeView, StadiumsView, TeamsView, NewsletterView, UpdateView
+from .views import HomeView, StadiumsView, TeamsView, NewsletterView, UpdateView, AboutView
 
 urlpatterns = (
     path("", HomeView.as_view(), name="home"),
     path("stadiums", StadiumsView.as_view(), name="stadiums"),
     path("teams", TeamsView.as_view(), name="teams"),
     path("newsletter", NewsletterView.as_view(), name="newsletter"),
+    path("about", AboutView.as_view(), name="about"),
     path("update", UpdateView.as_view(), name="update"),
     # Dans un cadre de projet réel, il serait préférable d'utiliser une URL moins prévisible que "admin"
     path("admin", admin.site.urls),
